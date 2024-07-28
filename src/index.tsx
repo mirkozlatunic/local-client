296ae79c865baf31e1ccb4450c8ff9173db738db
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-07-28 13:47:47 -0700

    start of project

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+  return <h1>hi</h1>;
+};
+
+root.render(<App />);
