d7a8202299f2d84ba6f9300a31d18aa83067f356
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-09 20:28:00 -0700

    installed monaco code editor

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
@@ -68,6 +69,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
