71e0811240c7abca28bf4eec5c49f86569d5872f
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-11 14:37:58 -0700

    setup of resizable component

---
+++ src/components/resizable.tsx
@@ -0,0 +1,17 @@
+import { ResizableBox } from "react-resizable";
+import "./resizable.css";
+
+interface ResizableProps {
+  direction: "horizontal" | "vertical";
+  children?: React.ReactNode;
+}
+
+const Resizeable: React.FC<ResizableProps> = ({ direction, children }) => {
+  return (
+    <ResizableBox height={300} width={300} resizeHandles={["s"]}>
+      {children}
+    </ResizableBox>
+  );
+};
+
+export default Resizeable;
