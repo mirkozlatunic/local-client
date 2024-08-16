f9b8a280c28c79adf542f449b0293b4fae8a2cd6
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-15 21:43:34 -0700

    rendering text and code cells

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,11 +1,20 @@
 import { Cell } from "../state";
+import CodeCell from "./code-cell";
+import TextEditor from "./text-editor";
 
 interface CellLsitItemProps {
   cell: Cell;
 }
 
 const CellListItem: React.FC<CellLsitItemProps> = ({ cell }) => {
-  return <div>{cell.id}</div>;
+  let child: JSX.Element;
+  if (cell.type === "code") {
+    child = <CodeCell />;
+  } else {
+    child = <TextEditor />;
+  }
+
+  return <div>{child}</div>;
 };
 
 export default CellListItem;
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,38 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import { ActionTypes } from "./action-types";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+store.dispatch({
+  type: ActionTypes.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "code",
+  },
+});
+
+store.dispatch({
+  type: ActionTypes.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "text",
+  },
+});
+
+store.dispatch({
+  type: ActionTypes.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "code",
+  },
+});
+
+store.dispatch({
+  type: ActionTypes.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "text",
+  },
+});
