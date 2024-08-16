5df8ca3e5254c58bd824b5c6894c2381d021bc32
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-16 16:29:59 -0700

    fixing key props

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
@@ -8,10 +9,10 @@ const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <>
+    <Fragment key={cell.id}>
       <AddCell nextCellId={cell.id} />
-      <CellListItem cell={cell} key={cell.id} />
-    </>
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
   return (
