0cfe8b9ce102a77c434d8ec8997bbb0c58bd45fc
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-16 16:28:13 -0700

    adding cells buttons

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,6 @@
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
+import AddCell from "./add-cell";
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -7,10 +8,17 @@ const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem cell={cell} key={cell.id} />
+    <>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} key={cell.id} />
+    </>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells} <AddCell nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
