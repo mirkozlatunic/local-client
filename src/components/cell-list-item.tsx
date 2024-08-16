cfed3ec4e48a79d730dc114b700c37a35163be57
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-15 21:36:19 -0700

    adding rendering function

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,5 +1,11 @@
-const CellListItem: React.FC = () => {
-  return <div>CellListItem</div>;
+import { Cell } from "../state";
+
+interface CellLsitItemProps {
+  cell: Cell;
+}
+
+const CellListItem: React.FC<CellLsitItemProps> = ({ cell }) => {
+  return <div>{cell.id}</div>;
 };
 
 export default CellListItem;
