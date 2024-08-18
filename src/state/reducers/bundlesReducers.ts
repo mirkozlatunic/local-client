b57763bc11801aef0db0269807e7b5c270cc59fc
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-17 22:57:43 -0700

    setup bundleReducer

--- src/state/reducers/bundlesReducers.ts
+++ src/state/reducers/bundlesReducers.ts
@@ -1,1 +1,29 @@
-export default 1;
+import { produce } from "immer";
+import { ActionTypes } from "../action-types";
+import { Action } from "../actions";
+
+interface BundlesState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  };
+}
+
+const intitialState: BundlesState = {};
+
+const reducer = produce(
+  (state: BundlesState = intitialState, action: Action): BundlesState => {
+    switch (action.type) {
+      case ActionTypes.BUNDLE_START:
+        return state;
+      case ActionTypes.BUNDLE_COMPLETE:
+        return state;
+      default:
+        return state;
+    }
+  },
+  intitialState
+);
+
+export default reducer;
