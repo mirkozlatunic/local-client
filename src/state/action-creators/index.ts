db817836b687f3782476f87f9713bc12e15b5e39
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-18 10:35:38 -0700

    fixing warning with useMemo

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -6,8 +6,6 @@ import {
   DeleteCellAction,
   MoveCellAction,
   InsertCellAfterAction,
-  BundleStartAction,
-  BundleCompleteAction,
   Direction,
 } from "../actions";
 import { CellType } from "../cell";
