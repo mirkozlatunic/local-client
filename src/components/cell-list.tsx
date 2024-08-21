8fb784e8bd440bbcbdda280f9428d0965547bda9
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-20 21:45:46 -0700

    margin styling for page

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -3,6 +3,7 @@ import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
 import "./code-cell.css";
+import "./cell-list.css";
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -17,7 +18,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
     </div>
